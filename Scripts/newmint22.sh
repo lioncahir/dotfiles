e997898aa11d3ebe0919ec2dd1a4dc64dc6cad83
lioncahir <miroslav.rosko@gmail.com>
2024-12-28 20:57:03 +0100

    Updates from 2024-12-28

---
+++ Scripts/newmint22.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+
+options=(\
+"Add NAS to /etc/fstab" \
+"Disable automatic printer discovery" \
+"Decrease swappiness" \
+"Perform software changes" \
+"Install flatpaks" \
+"Install fzf" \
+"Install Borg backup" \
+"Install lf file manager" \
+"Install JetBrains Mono Nerd font" \
+"Install Starship prompt"
+"Configure Cinnamon apps (Nemo, xed, xview)" \
+"Perform Cinnamon theming" \
+"Quit" \
+)
+
+add_nas () {
+    sudo mkdir -pv /media/NAS/Media
+    sudo groupadd -g 1500 nasmedia
+    sudo usermod -a -G nasmedia $USER
+    sudo cp /etc/fstab /etc/fstab.old
+    echo "//192.168.0.5/Media   /media/NAS/Media   cifs   guest,user,noauto,nofail,uid=$UID,gid=1500,file_mode=0770,dir_mode=0770,_netdev,iocharset=utf8   0   0" | sudo tee -a /etc/fstab
+    sudo mount -a
+}
+
+disable_printer_discovery () {
+    sudo systemctl stop cups-browsed
+    sudo systemctl disable cups-browsed
+}
+
+decrease_swappiness () {
+    echo "vm.swappiness = 20" | sudo tee -a /etc/sysctl.conf
+}
+
+software_changes () {
+    sudo add-apt-repository ppa:flatpak/stable
+    sudo add-apt-repository ppa:papirus/papirus
+    sudo apt update
+    sudo apt remove yaru-theme-icon onboard
+    sudo apt install vim stow grub2-theme-mint git fonts-crosextra-carlito fonts-crosextra-caladea eza gthumb mpv bat btop kitty
+    rm ~/.bashrc
+    cd ~/.dotfiles
+    stow -v bat btop kitty mint22 vim mpv
+}
+
+install_flatpaks () {
+    flatpak install org.gtk.Gtk3theme.Mint-Y-Dark
+    flatpak install com.github.johnfactotum.Foliate
+    flatpak install org.libreoffice.LibreOffice
+}
+
+install_borg () {
+    asset=""
+    asset=$(curl -s https://api.github.com/repos/borgbackup/borg/releases/latest | grep "browser_download_url.*borg-linux64\"" | cut -d : -f 2,3 | tr -d \")
+    if [ -n "$asset" ]
+    then
+        cd $(xdg-user-dir DOWNLOAD)
+        asset_file=$(basename $asset)
+        echo "Downloading $asset"
+        wget -q $asset
+        sudo cp $asset_file /usr/local/bin/borg
+        sudo chown root:root /usr/local/bin/borg
+        sudo chmod 755 /usr/local/bin/borg
+        sudo ln -sf /usr/local/bin/borg /usr/local/bin/borgfs
+        rm $asset_file
+        echo "Borg installed"
+    else
+        echo "Could not download Borg from GitHub"
+    fi
+}
+
+install_lf () {
+    asset=""
+    asset=$(curl -s https://api.github.com/repos/gokcehan/lf/releases/latest | grep "browser_download_url.*lf-linux-amd64.tar.gz\"" | cut -d : -f 2,3 | tr -d \")
+    if [ -n "$asset" ]
+    then
+        cd $(xdg-user-dir DOWNLOAD)
+        asset_file=$(basename $asset)
+        echo "Downloading $asset"
+        wget -q $asset
+        tar -xvzf $asset_file
+        sudo mv lf /usr/local/bin/
+        sudo chown root:root /usr/local/bin/lf
+        rm $asset_file 
+        echo "LF installed"
+    else
+        echo "Could not download LF from GitHub"
+    fi
+}
+
+install_fzf () {
+    asset=""
+    asset=$(curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | grep "browser_download_url.*linux_amd64.tar.gz\"" | cut -d : -f 2,3 | tr -d \")
+    if [ -n "$asset" ]
+    then
+        cd $(xdg-user-dir DOWNLOAD)
+        asset_file=$(basename $asset)
+        echo "Downloading $asset"
+        wget -q $asset
+        tar -xvzf $asset_file
+        sudo mv fzf /usr/local/bin/
+        sudo chown root:root /usr/local/bin/fzf
+        rm $asset_file
+        echo "fzf installed"
+    else
+        echo "Could not download fzf from GitHub"
+    fi
+}
+
+install_font () {
+    asset=""
+    asset=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep "browser_download_url.*JetBrainsMono.zip" | cut -d : -f 2,3 | tr -d \")
+    if [ -n "$asset" ]
+    then
+    	mkdir -pv ~/.local/share/fonts/JetBrainsMono
+        cd ~/.local/share/fonts/JetBrainsMono
+        asset_file=$(basename $asset)
+        echo "Downloading $asset"
+        wget -q $asset
+	    unzip -o $asset_file
+	    rm $asset_file
+	    fc-cache -fv
+        echo "JetBrains Mono font installed in $HOME/.local/share/fonts"
+    else
+        echo "Could not download JetBrains Mono font from GitHub"
+    fi
+}
+
+install_starship () {
+    curl -sS https://starship.rs/install.sh | sh
+}
+
+cinnamon_apps () {
+    gsettings set org.cinnamon.sounds login-enabled false
+    gsettings set org.cinnamon.sounds logout-enabled false
+
+    gsettings set org.nemo.desktop network-icon-visible false
+    gsettings set org.nemo.desktop computer-icon-visible false
+    gsettings set org.nemo.desktop volumes-visible false
+    gsettings set org.nemo.desktop trash-icon-visible false
+    gsettings set org.nemo.desktop home-icon-visible false
+
+    gsettings set org.nemo.list-view default-zoom-level 'smaller'
+    gsettings set org.nemo.preferences date-format 'iso'
+    gsettings set org.nemo.preferences default-folder-viewer 'icon-view'
+    gsettings set org.nemo.preferences default-sort-order 'type'
+    gsettings set org.nemo.preferences ignore-view-metadata true
+    gsettings set org.nemo.preferences inherit-show-thumbnails false
+    gsettings set org.nemo.preferences show-hidden-files false
+    gsettings set org.nemo.preferences show-image-thumbnails 'local-only'
+    gsettings set org.nemo.preferences show-location-entry true
+    gsettings set org.nemo.preferences show-show-thumbnails-toolbar false
+
+    gsettings set org.x.editor.preferences.editor bracket-matching false
+    gsettings set org.x.editor.preferences.editor display-line-numbers true
+    gsettings set org.x.editor.preferences.editor display-right-margin false
+    gsettings set org.x.editor.preferences.editor draw-whitespace false
+    gsettings set org.x.editor.preferences.editor draw-whitespace-inside false
+    gsettings set org.x.editor.preferences.editor draw-whitespace-leading false
+    gsettings set org.x.editor.preferences.editor draw-whitespace-newline false
+    gsettings set org.x.editor.preferences.editor draw-whitespace-trailing false
+    gsettings set org.x.editor.preferences.editor editor-font 'Monospace 12'
+    gsettings set org.x.editor.preferences.editor scheme 'oblivion'
+    gsettings set org.x.editor.preferences.editor highlight-current-line false
+    gsettings set org.x.editor.preferences.editor use-default-font false
+    gsettings set org.x.editor.preferences.ui minimap-visible false
+    gsettings set org.x.editor.preferences.ui statusbar-visible true
+
+    gsettings set org.x.viewer.ui image-gallery false
+    gsettings set org.x.viewer.ui sidebar false
+    gsettings set org.x.viewer.ui statusbar true
+    gsettings set org.x.viewer.ui toolbar true
+    gsettings set org.x.viewer.view background-color 'rgb(0,0,0)'
+    gsettings set org.x.viewer.view control-scroll-action 0
+    gsettings set org.x.viewer.view scroll-action 3
+    gsettings set org.x.viewer.view shift-control-scroll-action 5
+    gsettings set org.x.viewer.view shift-scroll-action 5
+    gsettings set org.x.viewer.view use-background-color true
+    gsettings set org.x.viewer.window maximized true
+}
+
+cinnamon_theming () {
+    gsettings set org.cinnamon.theme name 'Mint-Y-Dark'
+    gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark'
+    gsettings set org.cinnamon.desktop.interface icon-theme 'Papirus-Dark'
+    gsettings set org.cinnamon.desktop.wm.preferences theme 'Mint-Y'
+    papirus-folders -C paleorange --theme Papirus-Dark
+}
+
+
+clear
+
+echo -e "\033[1;32m"
+echo -e "*****************************************"
+echo -e "* Post-install script for Linux Mint 21 *"
+echo -e "*****************************************"
+echo -e "\033[0m"
+
+PS3="Choose an option: "
+COLUMNS=12
+select opt in "${options[@]}"; do
+    echo
+    case $REPLY in
+        1) add_nas ;;
+        2) disable_printer_discovery ;;
+        3) decrease_swappiness ;;
+        4) software_changes ;;
+        5) install_flatpaks ;;
+        6) install_fzf ;;
+        7) install_borg ;;
+        8) install_lf ;;
+        9) install_font ;;
+        10) install_starship ;;
+        11) cinnamon_apps ;;
+        12) cinnamon_theming ;;
+        13) break 2 ;;
+        *) echo "Invalid option" >&2
+    esac
+    REPLY=
+    COLUMNS=12
+    echo
+done
+
+
---
+++ mint22/.bashrc
@@ -0,0 +1,150 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Basic aliases
+alias ls='ls -lh --color=auto --group-directories-first'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+
+[[ "$(whoami)" = "root" ]] && return
+
+## Use the up and down arrow keys for finding a command in history
+## (you can write some initial letters of the command first).
+## Plus other shell options related to history
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+HISTCONTROL=ignoreboth
+HISTSIZE=1000
+HISTFILESIZE=2000
+shopt -s histappend
+shopt -s checkwinsize
+# If not running interactively, don't do anything 
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# alias for update checking
+alias aptupdate='sudo apt update && apt list --upgradeable'
+
+# alias for full update
+alias fullupdate='sudo apt update && sudo apt upgrade ; sudo apt autoremove ; flatpak update && flatpak remove --unused'
+
+# alias for flatpak list
+alias flatlist='flatpak list --columns=name,application,version,branch,size'
+
+# set man pager to BAT
+if command -v batcat &> /dev/null
+then
+    export BAT_THEME="Visual Studio Dark+"
+    export BAT_PAGER="less -r"
+    export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+    export MANROFFOPT="-c"
+    alias bat='batcat'
+fi
+
+# replace ls with exa of installed
+if command -v exa &> /dev/null
+then
+    alias ls='exa -lg --group-directories-first --sort=ext --icons'
+fi
+
+# one command to update dotfiles
+gitupdate () {
+    cd ~/.dotfiles
+    git add --all
+    git status
+    git commit -m "Updates from $(date '+%F')"
+    git push
+    cd $OLDPWD
+}
+
+# change working directory after quitting LF
+lf () {
+    cd "$(command lf -print-last-dir "$@")"
+}
+
+# set editor
+if command -v vim &> /dev/null
+then
+	export EDITOR="/usr/bin/vim"
+	export VISUAL="/usr/bin/vim"
+fi
+
+# set fzf options
+if command -v fzf &> /dev/null
+then
+	export FZF_DEFAULT_COMMAND='find . -mount ! -path '*/.cache/*' ! -path '*/.git/*' ! -path '*/.mozilla/*' 2>/dev/null'
+	export FZF_CTRL_T_COMMAND='find . -mount ! -path '*/.cache/*' ! -path '*/.git/*' ! -path '*/.mozilla/*' 2>/dev/null'
+	source /usr/share/fzf/key-bindings.bash
+    source /usr/share/fzf/completion.bash
+    alias fzf='fzf --cycle --info=inline'
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+if command -v starship &> /dev/null
+then
+    eval "$(starship init bash)"
+fi
+
