dd1fd67199b638ea266655db3f6740664e6eeff2
lioncahir <miroslav.rosko@gmail.com>
2025-01-11 13:44:54 +0100

    Updates from 2025-01-11

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -2,3 +2,9 @@ require("full-border"):setup {
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 }
+
+require("mime-ext"):setup {
+	-- If the mime-type is not in both filename and extension databases,
+	-- then fallback to Yazi's preset `mime` plugin, which uses `file(1)`
+	fallback_file1 = true,
+}
