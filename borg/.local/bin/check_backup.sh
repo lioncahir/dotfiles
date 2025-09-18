91dc6fba1d6665d1a7d73cc658ea5aee5a3e7a7b
lioncahir <miroslav.rosko@gmail.com>
2025-09-18 22:21:30 +0200

    Switch to Borgmatic

--- borg/.local/bin/check_backup.sh
+++ borg/.local/bin/check_backup.sh
@@ -2,6 +2,12 @@
 
 if [ -z $(find ~/ -maxdepth 1 -mtime -5 -name "NASBackup.log") ]
 then
-    ~/.local/bin/borg_backup.sh &>> ~/NASBackup.log
+#    ~/.local/bin/borg_backup.sh &>> ~/NASBackup.log
+    cp /etc/fstab ~/.config/
+    pacman -Qeq > ~/.config/packages.list 2>/dev/null
+    mount /media/NAS/Media
+
+    borgmatic --config ~/.config/borgmatic/config.yaml --verbosity 1 --list --stats &>> ~/BorgBase.log
+    borgmatic --config ~/.config/borgmatic/NAS.yaml --verbosity 1 --list --stats &>> ~/NASBackup.log
 fi
 
