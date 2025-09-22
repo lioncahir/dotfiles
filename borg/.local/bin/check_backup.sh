bd5d052ed73b50a52bf127078932b417f6557e67
lioncahir <miroslav.rosko@gmail.com>
2025-09-22 18:30:23 +0200

    Added start notification

--- borg/.local/bin/check_backup.sh
+++ borg/.local/bin/check_backup.sh
@@ -6,6 +6,7 @@ then
     cp /etc/fstab ~/.config/
     pacman -Qeq > ~/.config/packages.list 2>/dev/null
     mount /media/NAS/Media
+    notify-send --app-name="Borgmatic" "Starting Borgmatic backups"
 
     borgmatic --config ~/.config/borgmatic/config.yaml
     borgmatic --config ~/.config/borgmatic/NAS.yaml
