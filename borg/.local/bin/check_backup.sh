955ab812aa71058ffd27688b7ff0ba490e322349
lioncahir <miroslav.rosko@gmail.com>
2025-10-25 16:20:25 +0200

    Only start Borgmatic if credential file exists

--- borg/.local/bin/check_backup.sh
+++ borg/.local/bin/check_backup.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -z $(find ~/ -maxdepth 1 -mtime -5 -name "NASBackup.log") ]
-then
-#    ~/.local/bin/borg_backup.sh &>> ~/NASBackup.log
+if [[ -r "$HOME/.borg-passphrase" ]] && [[ -z $(find "$HOME" -maxdepth 1 -mtime -5 -name "NASBackup.log") ]]; then
     cp /etc/fstab ~/.config/
     pacman -Qeq > ~/.config/packages.list 2>/dev/null
     mount /media/NAS/Media
