ad3f0d6b86c93a30e6f1c5f68908e325e5a3dec2
lioncahir <miroslav.rosko@gmail.com>
2026-01-11 18:21:07 +0100

    Added force logic

--- borg/.local/bin/check_backup.sh
+++ borg/.local/bin/check_backup.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-if [[ -r "$HOME/.borg-passphrase" ]] && [[ -z $(find "$HOME" -maxdepth 1 -mtime -5 -name "NASBackup.log") ]]; then
-    cp /etc/fstab ~/.config/
-    pacman -Qeq > ~/.config/packages.list 2>/dev/null
-    mount /media/NAS/Media
-    notify-send --app-name="Borgmatic" "Starting Borgmatic backups"
+FORCE=false
+if [[ "$1" == "force" ]]; then
+    FORCE=true
+fi
 
-    borgmatic --config ~/.config/borgmatic/config.yaml
-    borgmatic --config ~/.config/borgmatic/NAS.yaml
+if [[ -r "$HOME/.borg-passphrase" ]]; then
+    if $FORCE || [[ -z $(find "$HOME" -maxdepth 1 -mtime -5 -name "NASBackup.log") ]]; then
+        cp /etc/fstab ~/.config/
+        pacman -Qeq > ~/.config/packages.list 2>/dev/null
+        mount /media/NAS/Media
+        notify-send --app-name="Borgmatic" "Starting Borgmatic backups"
+
+        borgmatic --config ~/.config/borgmatic/config.yaml
+        borgmatic --config ~/.config/borgmatic/NAS.yaml
+    fi
 fi
 
