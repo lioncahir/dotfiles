99403068cf33d768326f1f41c571156858bb82ad
lioncahir <miroslav.rosko@gmail.com>
2026-01-13 21:06:25 +0100

    Replace fnott with mako

--- borg/.local/bin/check_backup.sh
+++ borg/.local/bin/check_backup.sh
@@ -10,7 +10,7 @@ if [[ -r "$HOME/.borg-passphrase" ]]; then
         cp /etc/fstab ~/.config/
         pacman -Qeq > ~/.config/packages.list 2>/dev/null
         mount /media/NAS/Media
-        notify-send --app-name="Borgmatic" "Starting Borgmatic backups"
+        notify-send --icon="gtk-dialog-info" "Borgmatic" "Starting Borgmatic backups"
 
         borgmatic --config ~/.config/borgmatic/config.yaml
         borgmatic --config ~/.config/borgmatic/NAS.yaml
