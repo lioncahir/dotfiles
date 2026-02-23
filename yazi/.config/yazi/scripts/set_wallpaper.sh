d357fff34c107b7a0684fd50df89abf3ad0758ca
lioncahir <miroslav.rosko@gmail.com>
2026-02-23 15:52:30 +0100

    Updates from 2026-02-23

--- yazi/.config/yazi/scripts/set_wallpaper.sh
+++ yazi/.config/yazi/scripts/set_wallpaper.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 case $XDG_SESSION_DESKTOP in
-    "sway")     ln -sf "$1" ~/.wallpaper
-                swaymsg output "*" background ~/.wallpaper fill ;;
-    "i3")       feh --bg-scale $1 ;;
-    *)          echo "Can't do" ;;
+    "sway")     
+        ln -sf "$1" ~/.wallpaper
+        swaymsg output "*" background ~/.wallpaper fill ;;
+    "niri")
+        ln -sf "$1" ~/.wallpaper
+        PID=$(pidof swaybg)
+        niri msg action spawn-sh -- "swaybg -m fill -i ~/.wallpaper" 
+        sleep 1
+        kill "$PID" 2>/dev/null ;;
+    "i3")       
+        feh --bg-scale $1 ;;
+    *)  
+        echo "Can't do" ;;
 esac
--- yazi/.config/yazi/scripts/view_image.sh
+++ yazi/.config/yazi/scripts/view_image.sh
@@ -2,6 +2,7 @@
 
 case $XDG_SESSION_DESKTOP in
     "sway")     swayimg "$1" . ;;
+    "niri")     swayimg "$1" . ;;
     "i3")       feh --start-at "$1" ;;
     *)          xdg-open "$1" ;;
 esac
