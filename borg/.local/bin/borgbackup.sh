33c3df7c83c109894f1b101d409412bc68dc9bc1
lioncahir <miroslav.rosko@gmail.com>
2026-04-11 13:36:40 +0200

    Added comments and instructions

--- borg/.local/bin/borgbackup.sh
+++ borg/.local/bin/borgbackup.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Main launcher for Borgmatic backups, which executes backups to both BorgBase online repository
+# and to NAS. It is started from a systemd user unit.
+#
+# Prerequisites:
+# 1) all files correctly stowed (this script, Borgmatic config files, systemd user units)
+# 2) file ~/.borg-passphrase must exist and contain password to repositories
+# 3) systemd timer needs to be enabled with 'systemctl --user enable borgbackup.timer'
+#
+# Backup can be executed also manually by running 'systemctl --user start borgbackup.service'
+# or for individual repositories by running one of the borgmatic commands from this script.
+
 if [[ -r "$HOME/.borg-passphrase" ]]; then
     cp /etc/fstab ~/.config/
     pacman -Qeq > ~/.config/packages.list 2>/dev/null
