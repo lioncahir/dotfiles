693030e742388015f40daad4ad8d709a2ca8e67d
lioncahir <miroslav.rosko@gmail.com>
2026-04-21 18:56:02 +0200

    Adjusted Borgmatic configs to not require XDG_CONFIG_HOME as it's not always exposed when running as systemd user service

--- borg/.local/bin/borgbackup.sh
+++ borg/.local/bin/borgbackup.sh
@@ -5,21 +5,21 @@
 #
 # Prerequisites:
 # 1) all files correctly stowed (this script, Borgmatic config files, systemd user units)
-# 2) file ~/.borg-passphrase must exist and contain password to repositories
+# 2) file $HOME/.borg-passphrase must exist and contain password to repositories
 # 3) systemd timer needs to be enabled with 'systemctl --user enable borgbackup.timer'
 #
 # Backup can be executed also manually by running 'systemctl --user start borgbackup.service'
 # or for individual repositories by running one of the borgmatic commands from this script.
 
 if [[ -r "$HOME/.borg-passphrase" ]]; then
-    cp /etc/fstab ~/.config/
-    pacman -Qeq > ~/.config/packages.list 2>/dev/null
+    cp /etc/fstab $HOME/.config/
+    pacman -Qeq > $HOME/.config/packages.list 2>/dev/null
 
     notify-send --icon="gtk-dialog-info" "Borgmatic" "Starting Borgmatic backups"
-    borgmatic --config ~/.config/borgmatic/config.yaml
+    borgmatic --config $HOME/.config/borgmatic/config.yaml
 
     if mountpoint -q /media/NAS/Media || mount /media/NAS/Media; then
-        borgmatic --config ~/.config/borgmatic/NAS.yaml
+        borgmatic --config $HOME/.config/borgmatic/NAS.yaml
     else
         notify-send --icon="gtk-dialog-warning" --urgency=critical "Borgmatic" "NAS not reachable, skipping NAS backup"
     fi
