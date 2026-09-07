3fee4b3a1fa9251c654289b9521cb06296626378
lioncahir <miroslav.rosko@gmail.com>
2026-09-07 19:39:33 +0200

    Adjusted config based on upstream changes

--- swayimg/.config/swayimg/init.lua
+++ swayimg/.config/swayimg/init.lua
@@ -1,13 +1,13 @@
-swayimg.set_mode("viewer")  
-swayimg.enable_decoration(false)
+swayimg.mode = "viewer"  
+swayimg.decoration = 0
 
-swayimg.text.set_font("JetBrainsMono Nerd Font")
-swayimg.text.set_size(18)
-swayimg.text.set_foreground(0xffb4befe)
-swayimg.text.set_background(0x801e1e2e)
-swayimg.text.set_timeout(0)
+swayimg.text.font = "JetBrainsMono Nerd Font"
+swayimg.text.size = 18
+swayimg.text.color = 0xffb4befe
+swayimg.text.background = 0x801e1e2e
+swayimg.text.timeout = 0
 
-swayimg.viewer.set_default_scale("optimal")
+swayimg.viewer.default_scale = "optimal"
 swayimg.viewer.set_window_background(0xff1e1e2e)
 swayimg.viewer.set_text("topleft", {
   "File: {name}",
@@ -23,22 +23,22 @@ swayimg.viewer.set_text("bottomleft", {
 
 
 swayimg.viewer.on_key("q", function() swayimg.exit() end)
-swayimg.viewer.on_key("Home", function() swayimg.viewer.switch_image("first") end)
-swayimg.viewer.on_key("End", function() swayimg.viewer.switch_image("last") end)
-swayimg.viewer.on_key("Prior", function() swayimg.viewer.switch_image("prev") end)
-swayimg.viewer.on_key("Next", function() swayimg.viewer.switch_image("next") end)
-swayimg.viewer.on_key("f", function() swayimg.toggle_fullscreen() end)
+swayimg.viewer.on_key("Home", function() swayimg.viewer.open("first") end)
+swayimg.viewer.on_key("End", function() swayimg.viewer.open("last") end)
+swayimg.viewer.on_key("Prior", function() swayimg.viewer.open("prev") end)
+swayimg.viewer.on_key("Next", function() swayimg.viewer.open("next") end)
+swayimg.viewer.on_key("f", function() swayimg.fullscreen = not swayimg.fullscreen end)
 swayimg.viewer.on_key("0", function() swayimg.viewer.set_fix_scale("real") end)
 swayimg.viewer.on_key("w", function() swayimg.viewer.set_fix_scale("width") end)
-swayimg.viewer.on_key("i", function() swayimg.text.hide() end)
+swayimg.viewer.on_key("i", function() swayimg.text.visible = not swayimg.text.visible end)
 swayimg.viewer.on_key("Shift+i", function() swayimg.text.show() end)
 swayimg.viewer.on_key("slash", function() swayimg.viewer.set_fix_scale("fit") end)
 swayimg.viewer.on_key("period", function() swayimg.viewer.set_fix_scale("real") end)
 swayimg.viewer.on_key("1", function() swayimg.viewer.set_abs_scale(1.0) end)
 swayimg.viewer.on_key("2", function() swayimg.viewer.set_abs_scale(2.0) end)
 
-swayimg.viewer.on_mouse("ScrollUp", function() swayimg.viewer.switch_image("prev") end)
-swayimg.viewer.on_mouse("ScrollDown", function() swayimg.viewer.switch_image("next") end)
+swayimg.viewer.on_mouse("ScrollUp", function() swayimg.viewer.open("prev") end)
+swayimg.viewer.on_mouse("ScrollDown", function() swayimg.viewer.open("next") end)
 
 swayimg.viewer.on_key("Left", function()
   local wnd = swayimg.get_window_size()
